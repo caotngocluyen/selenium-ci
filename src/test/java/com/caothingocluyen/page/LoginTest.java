e27a6dd84105e5a6b10f53773916e40d02eb364f
Caotngocluyen Caotngocluyen <caotngocluyen@gmail.com>
2026-06-08 15:26:08 +0700

    Add headless Chrome for CI

--- src/test/java/com/caothingocluyen/page/LoginTest.java
+++ src/test/java/com/caothingocluyen/page/LoginTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -20,7 +21,13 @@ public class LoginTest {
 
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+options.addArguments("--headless");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+
+driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
 
